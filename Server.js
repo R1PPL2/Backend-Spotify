61bd48be3bba6b5258b65fab884d71ccd38513db
Dennis Kuijpers <dennis@Denniss-MacBook-Air.local>
2025-02-19 04:22:43 +0700

    last

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ require ('dotenv') .config ()
 
 app.use (cors (
     {
-        origin: 'http://localhost:3000',
+        origin: 'https://r1ppl2.github.io/Spotify-Front/',
         credentials: true,
     }
 ))
@@ -20,7 +20,7 @@ app.use (cookieParser ())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cors())
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://r1ppl2.github.io/Spotify-Front/' }));
 
 
 app.use((req, res, next) => {
